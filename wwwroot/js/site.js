e9fe2e6cc76b20c73884c83328a8ebd1876909f1
Vedat Ersin Ceviz <vedatersinceviz@gmail.com>
2026-03-28 01:56:33 +0300

    dosya temizliği

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,9 +1,5 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
 document.addEventListener('DOMContentLoaded', () => {
-    // Smooth scroll for category links
     document.querySelectorAll('.cat-icon-card, .nav-link').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
             const href = this.getAttribute('href');
