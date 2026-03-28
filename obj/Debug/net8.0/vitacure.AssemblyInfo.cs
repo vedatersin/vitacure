0da449df7f24d1ebad5b3576f7136994fe4f89e5
Vedat Ersin Ceviz <vedatersinceviz@gmail.com>
2026-03-28 03:10:12 +0300

    alt kategori düzenlemesi UI son

--- obj/Debug/net8.0/vitacure.AssemblyInfo.cs
+++ obj/Debug/net8.0/vitacure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("vitacure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abeb5b8dc7260187766ab2e283b45c67b86c3356")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9fe2e6cc76b20c73884c83328a8ebd1876909f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("vitacure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("vitacure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
